2db39b3879448a481ddcb903d3f6556a066bafb8
AtherOmar-17 <98719815+AtherOmar-17@users.noreply.github.com>
2022-08-02 00:45:22 -0400

    x64 support

--- QuickSDL/SDLTutorial/main.cpp
+++ QuickSDL/SDLTutorial/main.cpp
@@ -1,6 +1,10 @@
 #include "GameManager.h"
 
+#if defined(_WIN64)
+int wmain(int argc, char* argv[]) {
+#else
 int main(int argc, char* argv[]) {
+#endif
 
 	QuickSDL::GameManager* game = QuickSDL::GameManager::Instance();
 
